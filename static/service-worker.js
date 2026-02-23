6e0289098ba785cba2d7afaf41da168bc6d16ddd
tkgorira <GitHub>
2026-02-23 20:33:14 +0900

    Add PWA manifest, service worker and icons

---
+++ static/service-worker.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = "mainichi-menu-cache-v1";
+const URLS_TO_CACHE = [
+  "/",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
